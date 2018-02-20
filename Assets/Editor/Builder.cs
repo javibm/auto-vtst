97d5e79abb997d9e4e670717d35de9850f64554e
Javier Agüera <javier.cazorla@labcavegames.com>
2018-02-20 10:12:28 +0100

    first commit

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Builder : MonoBehaviour 
+{
+	[MenuItem("Test/Build")]
+	static void PerformBuild ()
+	{
+		BuildPipeline.BuildPlayer(getScenesInBuildSettings(), Path.Combine(Path.GetFullPath("."), "test.apk"), BuildTarget.Android, new BuildOptions{});
+	}
+
+	private static string[] getScenesInBuildSettings()
+    {
+      List<string> temp = new List<string>();
+      foreach (UnityEditor.EditorBuildSettingsScene scene in UnityEditor.EditorBuildSettings.scenes)
+      {
+        if (scene.enabled)
+        {
+          temp.Add(scene.path);
+        }
+      }
+      return temp.ToArray();
+    }
+}
