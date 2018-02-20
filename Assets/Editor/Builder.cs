44ef36ba893c94c3843b9c55c3052cde64f7440b
Javier Agüera <javier.cazorla@labcavegames.com>
2018-02-20 12:37:22 +0100

    Fifth commit

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -10,6 +10,12 @@ public class Builder : MonoBehaviour
 	[MenuItem("Test/Build")]
 	static void PerformBuild ()
 	{
+		var moduleManager = System.Type.GetType("UnityEditor.Modules.ModuleManager,UnityEditor.dll");
+		var isPlatformSupportLoaded = moduleManager.GetMethod("IsPlatformSupportLoaded", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+		var getTargetStringFromBuildTarget = moduleManager.GetMethod("GetTargetStringFromBuildTarget", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+			
+		Debug.Log("JAVI "+ (bool)isPlatformSupportLoaded.Invoke(null,new object[] {(string)getTargetStringFromBuildTarget.Invoke(null, new object[] {BuildTarget.Android})}));
+
 		EditorPrefs.SetString("AndroidSdkRoot", Environment.GetEnvironmentVariable("ANDROID_SDK_HOME"));
 		EditorPrefs.SetString("JdkPath", Environment.GetEnvironmentVariable("JAVA_HOME"));
 		EditorUserBuildSettings.selectedBuildTargetGroup = BuildTargetGroup.Android;
