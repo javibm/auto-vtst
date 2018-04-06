6679a08f7168a08a78357e33ded2b0bcc7246d12
Javier Agüera Cazorla <javier.cazorla@labcavegames.com>
2018-04-06 13:34:05 +0200

    Update Application Exit

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,42 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using System.IO;
-using System;
+using UnityEditor;
+using UnityEngine;
 
-public class Builder : MonoBehaviour 
-{
-	[MenuItem("Test/Build")]
-	static void PerformBuild ()
-	{
-		var moduleManager = System.Type.GetType("UnityEditor.Modules.ModuleManager,UnityEditor.dll");
-		var isPlatformSupportLoaded = moduleManager.GetMethod("IsPlatformSupportLoaded", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-		var getTargetStringFromBuildTarget = moduleManager.GetMethod("GetTargetStringFromBuildTarget", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-			
-		Debug.Log("JAVI "+ (bool)isPlatformSupportLoaded.Invoke(null,new object[] {(string)getTargetStringFromBuildTarget.Invoke(null, new object[] {BuildTarget.Android})}));
+public class Builder : MonoBehaviour {
+	[MenuItem ("Test/Build")]
+	static void PerformBuild () {
+		var moduleManager = System.Type.GetType ("UnityEditor.Modules.ModuleManager,UnityEditor.dll");
+		var isPlatformSupportLoaded = moduleManager.GetMethod ("IsPlatformSupportLoaded", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+		var getTargetStringFromBuildTarget = moduleManager.GetMethod ("GetTargetStringFromBuildTarget", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
 
-		EditorPrefs.SetString("AndroidSdkRoot", Environment.GetEnvironmentVariable("ANDROID_SDK_HOME"));
-		EditorPrefs.SetString("JdkPath", Environment.GetEnvironmentVariable("JAVA_HOME"));
-		AssetDatabase.SaveAssets();
-		string error = BuildPipeline.BuildPlayer(getScenesInBuildSettings(), Path.Combine(Path.GetFullPath("."), "test.apk"), BuildTarget.Android, new BuildOptions{});
-		Debug.Log("JAVI " + error);
-		if( string.IsNullOrEmpty( error ) )
-		    EditorApplication.Exit( 0 );
-		else
-		    EditorApplication.Exit( 1 );
+		Debug.Log ("JAVI " + (bool) isPlatformSupportLoaded.Invoke (null, new object[] {
+			(string) getTargetStringFromBuildTarget.Invoke (null, new object[] { BuildTarget.Android }) }));
+
+		EditorPrefs.SetString ("AndroidSdkRoot", Environment.GetEnvironmentVariable ("ANDROID_SDK_HOME"));
+		EditorPrefs.SetString ("JdkPath", Environment.GetEnvironmentVariable ("JAVA_HOME"));
+		AssetDatabase.SaveAssets ();
+		BuildPipeline.BuildPlayer (getScenesInBuildSettings (), Path.Combine (Path.GetFullPath ("."), "test.apk"), BuildTarget.Android, new BuildOptions { });
 	}
 
-	private static string[] getScenesInBuildSettings()
-    {
-      List<string> temp = new List<string>();
-      foreach (UnityEditor.EditorBuildSettingsScene scene in UnityEditor.EditorBuildSettings.scenes)
-      {
-        if (scene.enabled)
-        {
-          temp.Add(scene.path);
-        }
-      }
-      return temp.ToArray();
-    }
+	private static string[] getScenesInBuildSettings () {
+		List<string> temp = new List<string> ();
+		foreach (UnityEditor.EditorBuildSettingsScene scene in UnityEditor.EditorBuildSettings.scenes) {
+			if (scene.enabled) {
+				temp.Add (scene.path);
+			}
+		}
+		return temp.ToArray ();
+	}
 }
